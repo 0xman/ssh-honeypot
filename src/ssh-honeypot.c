96671acf21e66f89a6dae7d126c152779fd7a8db
0xman <miraip0ts@protonmail.com>
2019-09-28 12:28:43 -0700

    Update ssh-honeypot.c

--- src/ssh-honeypot.c
+++ src/ssh-honeypot.c
@@ -1,4 +1,5 @@
-/* ssh-honeypot -- by Daniel Roberson (daniel(a)planethacker.net) 2016-2017 
+/* ssh-honeypot -- by Daniel Roberson (daniel(a)planethacker.net) 2016-2017 - removed some log features - to output only username&passwords
+use : awk '{print $7,$8}' ssh-honeypot.log > passlist
  */
 
 #include <stdio.h>
