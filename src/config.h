0b3f2ca660c2a7e4c328c3f48f44b025181f744d
0xman <miraip0ts@protonmail.com>
2019-09-28 12:36:29 -0700

    Update config.h

--- src/config.h
+++ src/config.h
@@ -3,6 +3,6 @@
 
 #define LOGFILE  "ssh-honeypot.log"              /* default log location */
 #define PIDFILE  "ssh-honeypot.pid"              /* default pid file location */
-#define PORT     22                              /* default port */
+#define PORT     22                              /* default port - change the ssh port on your own server to listen to something else then port 22
 #define RSAKEY   "ssh-honeypot.rsa"              /* default RSA key */
 #define BINDADDR "0.0.0.0"                       /* default bind address */
